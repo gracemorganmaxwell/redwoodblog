db2cd052d476df358a36cc5ac90a93b28211584c
gracemorganmaxwell@gmail.com <gracie.morganmaxwell@gmail.com>
2023-03-20 20:26:48 +1300

    contact page added

---
+++ web/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,79 @@
+import {
+  Form,
+  TextField,
+  TextAreaField,
+  Submit,
+  FieldError,
+  Label,
+} from '@redwoodjs/forms'
+import { MetaTags, useMutation } from '@redwoodjs/web'
+import { Toaster, toast } from '@redwoodjs/web/dist/toast'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
+
+const ContactPage = () => {
+  const [create, { loading }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      toast.success('Thank you for your message!')
+    },
+  })
+
+  const onSubmit = (data) => {
+    console.log(data)
+    create({
+      variables: {
+        input: data,
+      },
+    })
+  }
+
+  return (
+    <>
+      <MetaTags title="Contact" description="Contact page" />
+
+      <Form onSubmit={onSubmit}>
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          errorClassName="error"
+          validation={{ required: true }}
+        />
+        <FieldError name="name" className="error" />
+
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          errorClassName="error"
+          validation={{
+            required: true,
+            pattern: { value: /^[^@]+@[^.]\..+$/ },
+          }}
+        />
+        <FieldError name="email" className="error" />
+
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          errorClassName="error"
+          validation={{ required: true }}
+        />
+        <FieldError name="message" className="error" />
+        <Submit disabled={loading}>Send Message</Submit>
+      </Form>
+    </>
+  )
+}
+
+export default ContactPage
